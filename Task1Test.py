f23115b215c6ac9c7e172336b5a396833b68508c
Unknown <2023170400@cis.asu.edu.eg>
2025-10-13 09:01:57 +0300

    added test

--- Task1Test.py
+++ Task1Test.py
@@ -1,122 +1,95 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
-
-
 def ReadSignalFile(file_name):
-    expected_indices=[]
-    expected_samples=[]
+    expected_indices = []
+    expected_samples = []
     with open(file_name, 'r') as f:
-        line = f.readline()
-        line = f.readline()
-        line = f.readline()
-        line = f.readline()
-        while line:
-            # process line
-            L=line.strip()
-            if len(L.split(' '))==2:
-                L=line.split(' ')
-                V1=int(L[0])
-                V2=float(L[1])
-                expected_indices.append(V1)
-                expected_samples.append(V2)
-                line = f.readline()
+        for line in f:
+            L = line.strip()
+            if len(L.split()) == 2:
+                V1, V2 = L.split()
+                expected_indices.append(int(V1))
+                expected_samples.append(float(V2))
             else:
                 break
-    return expected_indices,expected_samples
+    return expected_indices, expected_samples
 
 
-# In[ ]:
 
+def AddSignalSamplesAreEqual(userFirstSignal, userSecondSignal, Your_indices, Your_samples):
+    if userFirstSignal == 'Signal1.txt' and userSecondSignal == 'Signal2.txt':
+        file_name = "tests/task1/Signal1+signal2.txt"
+    elif userFirstSignal == 'Signal1.txt' and userSecondSignal == 'Signal3.txt':
+        file_name = "tests/task1/signal1+signal3.txt"
 
-def AddSignalSamplesAreEqual(userFirstSignal,userSecondSignal,Your_indices,Your_samples):
-    if(userFirstSignal=='Signal1.txt' and userSecondSignal=='Signal2.txt'):
-        file_name="" # write here path of signal1+signal2
-    elif(userFirstSignal=='Signal1.txt' and userSecondSignal=='Signal3.txt'):
-        file_name="" # write here path of signal1+signal3
-    expected_indices,expected_samples=ReadSignalFile(file_name)          
-    if (len(expected_samples)!=len(Your_samples)) and (len(expected_indices)!=len(Your_indices)):
+    expected_indices, expected_samples = ReadSignalFile(file_name)
+    if (len(expected_samples) != len(Your_samples)) or (len(expected_indices) != len(Your_indices)):
         print("Addition Test case failed, your signal have different length from the expected one")
         return
     for i in range(len(Your_indices)):
-        if(Your_indices[i]!=expected_indices[i]):
-            print("Addition Test case failed, your signal have different indicies from the expected one") 
+        if Your_indices[i] != expected_indices[i]:
+            print("Addition Test case failed, your signal have different indicies from the expected one")
             return
     for i in range(len(expected_samples)):
         if abs(Your_samples[i] - expected_samples[i]) < 0.01:
             continue
         else:
-            print("Addition Test case failed, your signal have different values from the expected one") 
+            print("Addition Test case failed, your signal have different values from the expected one")
             return
     print("Addition Test case passed successfully")
 
 
-# In[ ]:
+def MultiplySignalByConst(User_Const, Your_indices, Your_samples):
+    if User_Const == 5:
+        file_name = "tests/task1/MultiplySignalByConstant-Signal1 - by 5.txt"
+    elif User_Const == 10:
+        file_name = "tests/task1/MultiplySignalByConstant-signal2 - by 10.txt"
 
-
-def MultiplySignalByConst(User_Const,Your_indices,Your_samples):
-    if(User_Const==5):
-        file_name="" # write here path of MultiplySignalByConstant-Signal1 - by 5.txt
-    elif(User_Const==10):
-        file_name="" # write here path of MultiplySignalByConstant-Signal2 - by 10.txt
-        
-    expected_indices,expected_samples=ReadSignalFile(file_name)      
-    if (len(expected_samples)!=len(Your_samples)) and (len(expected_indices)!=len(Your_indices)):
-        print("Multiply by "+User_Const.str()+ " Test case failed, your signal have different length from the expected one")
+    expected_indices, expected_samples = ReadSignalFile(file_name)
+    if (len(expected_samples) != len(Your_samples)) or (len(expected_indices) != len(Your_indices)):
+        print("Multiply by " + str(User_Const) + " Test case failed, your signal have different length from the expected one")
         return
     for i in range(len(Your_indices)):
-        if(Your_indices[i]!=expected_indices[i]):
-            print("Multiply by "+User_Const.str()+" Test case failed, your signal have different indicies from the expected one") 
+        if Your_indices[i] != expected_indices[i]:
+            print("Multiply by " + str(User_Const) + " Test case failed, your signal have different indicies from the expected one")
             return
     for i in range(len(expected_samples)):
         if abs(Your_samples[i] - expected_samples[i]) < 0.01:
             continue
         else:
-            print("Multiply by "+User_Const.str()+" Test case failed, your signal have different values from the expected one") 
+            print("Multiply by " + str(User_Const) + " Test case failed, your signal have different values from the expected one")
             return
-    print("Multiply by "+User_Const.str()+" Test case passed successfully")
+    print("Multiply by " + str(User_Const) + " Test case passed successfully")
 
 
-# In[ ]:
-
-
-#TaskName => choose it (string explain the name of task like (adding sig1+sig2,subtracting, .... etc.))
-#output_file_name => output file path (output file given by TAs)
-# Your_indices => your indices list from your code (generated/calculated by you)
-# Your_samples => your samples list from your code (generated/calculated by you)
-def SignalSamplesAreEqual(TaskName,output_file_name,Your_indices,Your_samples):
-    expected_indices=[]
-    expected_samples=[]
+def SignalSamplesAreEqual(TaskName, output_file_name, Your_indices, Your_samples):
+    expected_indices = []
+    expected_samples = []
     with open(output_file_name, 'r') as f:
         line = f.readline()
-        line = f.readline()
-        line = f.readline()
-        line = f.readline()
         while line:
-            # process line
-            L=line.strip()
-            if len(L.split(' '))==2:
-                L=line.split(' ')
-                V1=int(L[0])
-                V2=float(L[1])
+            L = line.strip()
+            if len(L.split(' ')) == 2:
+                L = line.split(' ')
+                V1 = int(L[0])
+                V2 = float(L[1])
                 expected_indices.append(V1)
                 expected_samples.append(V2)
                 line = f.readline()
             else:
                 break
-    if (len(expected_samples)!=len(Your_samples)) and (len(expected_indices)!=len(Your_indices)):
-         print(TaskName+" Test case failed, your signal have different length from the expected one")
-         return
+    if (len(expected_samples) != len(Your_samples)) or (len(expected_indices) != len(Your_indices)):
+        print(TaskName + " Test case failed, your signal have different length from the expected one")
+        return
     for i in range(len(Your_indices)):
-        if(Your_indices[i]!=expected_indices[i]):
-            print(TaskName+" Test case failed, your signal have different indicies from the expected one") 
-            return             
+        if Your_indices[i] != expected_indices[i]:
+            print(TaskName + " Test case failed, your signal have different indicies from the expected one")
+            return
     for i in range(len(expected_samples)):
         if abs(Your_samples[i] - expected_samples[i]) < 0.01:
             continue
         else:
-            print(TaskName+" Test case failed, your signal have different values from the expected one") 
+            print(TaskName + " Test case failed, your signal have different values from the expected one")
             return
-    print(TaskName+" Test case passed successfully")
-
+    print(TaskName + " Test case passed successfully")
--- Task2Test.py
+++ Task2Test.py
@@ -1,164 +1,127 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
-
-
 def ReadSignalFile(file_name):
-    expected_indices=[]
-    expected_samples=[]
+    expected_indices = []
+    expected_samples = []
     with open(file_name, 'r') as f:
-        line = f.readline()
-        line = f.readline()
-        line = f.readline()
-        line = f.readline()
+        for line in f:
+            L = line.strip()
+            if len(L.split()) == 2:
+                V1, V2 = L.split()
+                expected_indices.append(int(V1))
+                expected_samples.append(float(V2))
+            else:
+                break
+    return expected_indices, expected_samples
+
+
+
+def SinCosSignalSamplesAreEqual(user_choice, file_name, indices, samples):
+    expected_indices = []
+    expected_samples = []
+    with open(file_name, 'r') as f:
+        line = f.readlines()
         while line:
-            # process line
-            L=line.strip()
-            if len(L.split(' '))==2:
-                L=line.split(' ')
-                V1=int(L[0])
-                V2=float(L[1])
+            L = line.strip()
+            if len(L.split(' ')) == 2:
+                L = line.split(' ')
+                V1 = int(L[0])
+                V2 = float(L[1])
                 expected_indices.append(V1)
                 expected_samples.append(V2)
                 line = f.readline()
             else:
                 break
-    return expected_indices,expected_samples
 
-
-# In[ ]:
-
-
-def SinCosSignalSamplesAreEqual(user_choice,file_name,indices,samples):
-    expected_indices=[]
-    expected_samples=[]
-    with open(file_name, 'r') as f:
-        line = f.readline()
-        line = f.readline()
-        line = f.readline()
-        line = f.readline()
-        while line:
-            # process line
-            L=line.strip()
-            if len(L.split(' '))==2:
-                L=line.split(' ')
-                V1=int(L[0])
-                V2=float(L[1])
-                expected_indices.append(V1)
-                expected_samples.append(V2)
-                line = f.readline()
-            else:
-                break
-                
-    if len(expected_samples)!=len(samples):
-        print(user_choice+" Test case failed, your signal have different length from the expected one")
+    if len(expected_samples) != len(samples):
+        print(user_choice + " Test case failed, your signal have different length from the expected one")
         return
     for i in range(len(expected_samples)):
         if abs(samples[i] - expected_samples[i]) < 0.01:
             continue
         else:
-            print(user_choice+" Test case failed, your signal have different values from the expected one") 
+            print(user_choice + " Test case failed, your signal have different values from the expected one")
             return
-    print(user_choice +" Test case passed successfully")
+    print(user_choice + " Test case passed successfully")
 
 
-# In[ ]:
+def SubSignalSamplesAreEqual(userFirstSignal, userSecondSignal, Your_indices, Your_samples):
+    if userFirstSignal == 'Signal1.txt' and userSecondSignal == 'Signal2.txt':
+        file_name = "tests/task2/signal1-signal2.txt"  # write here path of signal1-signal2
+    elif userFirstSignal == 'Signal1.txt' and userSecondSignal == 'Signal3.txt':
+        file_name = "tests/task2/signal1-signal3.txt"  # write here path of signal1-signal3
 
+    expected_indices, expected_samples = ReadSignalFile(file_name)
 
-def SubSignalSamplesAreEqual(userFirstSignal,userSecondSignal,Your_indices,Your_samples):
-    if(userFirstSignal=='Signal1.txt' and userSecondSignal=='Signal2.txt'):
-        file_name="" # write here path of signal1-signal2
-    elif(userFirstSignal=='Signal1.txt' and userSecondSignal=='Signal3.txt'):
-        file_name="" # write here path of signal1-signal3
-        
-    expected_indices,expected_samples=ReadSignalFile(file_name)   
-    
-    if (len(expected_samples)!=len(Your_samples)) and (len(expected_indices)!=len(Your_indices)):
+    if (len(expected_samples) != len(Your_samples)) or (len(expected_indices) != len(Your_indices)):
         print("Subtraction Test case failed, your signal have different length from the expected one")
         return
     for i in range(len(Your_indices)):
-        if(Your_indices[i]!=expected_indices[i]):
-            print("Subtraction Test case failed, your signal have different indicies from the expected one") 
+        if Your_indices[i] != expected_indices[i]:
+            print("Subtraction Test case failed, your signal have different indicies from the expected one")
             return
     for i in range(len(expected_samples)):
         if abs(Your_samples[i] - expected_samples[i]) < 0.01:
             continue
         else:
-            print("Subtraction Test case failed, your signal have different values from the expected one") 
+            print("Subtraction Test case failed, your signal have different values from the expected one")
             return
     print("Subtraction Test case passed successfully")
 
 
-# In[ ]:
+def NormalizeSignal(MinRange, MaxRange, Your_indices, Your_samples):
+    if MinRange == -1 and MaxRange == 1:
+        file_name = "/tests/task2/normalize of signal 1 (from -1 to 1)-- output.txt"
+    elif MinRange == 0 and MaxRange == 1:
+        file_name = "/tests/task2/normlize signal 2 (from 0 to 1 )-- output.txt"
 
+    expected_indices, expected_samples = ReadSignalFile(file_name)
 
-def NormalizeSignal(MinRange,MaxRange,Your_indices,Your_samples):
-    if(MinRange==-1 and MaxRange==1):
-        file_name="" # write here path of normalize signal 1 output.txt
-    elif(MinRange==-1 and MaxRange==1):
-        file_name="" # write here path of normalize signal 2 output.txt
-        
-    expected_indices,expected_samples=ReadSignalFile(file_name)      
-    
-    if (len(expected_samples)!=len(Your_samples)) and (len(expected_indices)!=len(Your_indices)):
+    if (len(expected_samples) != len(Your_samples)) or (len(expected_indices) != len(Your_indices)):
         print("Normalization Test case failed, your signal have different length from the expected one")
         return
     for i in range(len(Your_indices)):
-        if(Your_indices[i]!=expected_indices[i]):
-            print("Normalization Test case failed, your signal have different indicies from the expected one") 
+        if Your_indices[i] != expected_indices[i]:
+            print("Normalization Test case failed, your signal have different indicies from the expected one")
             return
     for i in range(len(expected_samples)):
         if abs(Your_samples[i] - expected_samples[i]) < 0.01:
             continue
         else:
-            print("Normalization Test case failed, your signal have different values from the expected one") 
+            print("Normalization Test case failed, your signal have different values from the expected one")
             return
     print("Normalization Test case passed successfully")
 
 
-# In[ ]:
-
-
-# use this twice one for Accumlation and one for Squaring
-# Task name when call ACC or SQU
-
-
-#TaskName => choose it (string explain the name of task like (adding,subtracting, .... etc.))
-#output_file_name => output file path (given by TAs)
-# Your_indices => your indices list from your code (generated/calculated by you)
-# Your_samples => your samples list from your code (generated/calculated by you)
-def SignalSamplesAreEqual(TaskName,output_file_name,Your_indices,Your_samples):
-    expected_indices=[]
-    expected_samples=[]
+def SignalSamplesAreEqual(TaskName, output_file_name, Your_indices, Your_samples):
+    expected_indices = []
+    expected_samples = []
     with open(output_file_name, 'r') as f:
-        line = f.readline()
-        line = f.readline()
-        line = f.readline()
-        line = f.readline()
+        line = f.readlines()
         while line:
-            # process line
-            L=line.strip()
-            if len(L.split(' '))==2:
-                L=line.split(' ')
-                V1=int(L[0])
-                V2=float(L[1])
+            L = line.strip()
+            if len(L.split(' ')) == 2:
+                L = line.split(' ')
+                V1 = int(L[0])
+                V2 = float(L[1])
                 expected_indices.append(V1)
                 expected_samples.append(V2)
                 line = f.readline()
             else:
-                break
-    if (len(expected_samples)!=len(Your_samples)) and (len(expected_indices)!=len(Your_indices)):
-         print(TaskName+" Test case failed, your signal have different length from the expected one")
+                line = f.readline()
+                continue
+    if (len(expected_samples) != len(Your_samples)) or (len(expected_indices) != len(Your_indices)):
+        print(TaskName + " Test case failed, your signal have different length from the expected one")
+        return
     for i in range(len(Your_indices)):
-        if(Your_indices[i]!=expected_indices[i]):
-            print(TaskName+" Test case failed, your signal have different indicies from the expected one") 
-            return             
+        if Your_indices[i] != expected_indices[i]:
+            print(TaskName + " Test case failed, your signal have different indicies from the expected one")
+            return
     for i in range(len(expected_samples)):
         if abs(Your_samples[i] - expected_samples[i]) < 0.01:
             continue
         else:
-            print(TaskName+" Test case failed, your signal have different values from the expected one") 
+            print(TaskName + " Test case failed, your signal have different values from the expected one")
             return
-    print(TaskName+" Test case passed successfully")
-
+    print(TaskName + " Test case passed successfully")
